543bff720417d45d63019173533b9f9ffcf1e1cf
Abadi11 <a.salman2040@gmail.com>
2022-11-23 11:08:30 +0000

    Header test

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,8 +1,42 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Header from '../Header';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('should render same text passed into title prop', () => {
+  render(<Header title="About" />);
+  const headingElement = screen.getByText(/about/i);
+  expect(headingElement).toBeInTheDocument();
 });
+
+// it('should render with the same role heading', () => {
+//     render(<Header title="About" />);
+//     const headingElement = screen.getByRole("heading", { name: "About" });
+//     expect(headingElement).toBeInTheDocument();
+//   });
+
+// it('should render with the same title Header', () => {
+// render(<Header title="About" />);
+// const headingElement = screen.getByTitle("Header");
+// expect(headingElement).toBeInTheDocument();
+// });
+
+// it('should render with the same test ID', () => {
+//     render(<Header title="About" />);
+//     const headingElement = screen.getByTestId("header-1");
+//     expect(headingElement).toBeInTheDocument();
+// });
+
+// // find
+
+// it('should render same text passed into title prop by find method', async () => {
+//     render(<Header title="About" />);
+//     const headingElement = await screen.findByText(/about/i);
+//     expect(headingElement).toBeInTheDocument();
+// });
+  
+// //getAll
+
+// it('should render same text passed into title prop by get all', async () => {
+//     render(<Header title="About" />);
+//     const headingElements =  screen.getAllByRole("heading");
+//     expect(headingElements.length).toBe(2);
+// });
